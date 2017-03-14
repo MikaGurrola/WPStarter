92ab3280a14e0ed9529dc3c32fde85d0d1cafc72
Mika Gurrola <mikagurrola90@gmail.com>
2017-03-13 21:01:36 -0400

    fixed url path for styles

--- public/wp-content/themes/underscores/functions.php
+++ public/wp-content/themes/underscores/functions.php
@@ -107,9 +107,9 @@ add_action( 'widgets_init', 'underscores_widgets_init' );
 function underscores_scripts() {
 	wp_enqueue_style( 'underscores-style', get_stylesheet_uri() );
 
-	wp_enqueue_style('style', '/wp-content/themes/three21/library/css/main.css', array(), 'underscores');
+	wp_enqueue_style('style', '/wp-content/themes/underscores/library/css/main.css', array(), 'underscores');
 
-	wp_enqueue_script('main', '/wp-content/themes/three21/library/js/main.js', array('jquery'), 'underscores', false);
+	wp_enqueue_script('main', '/wp-content/themes/underscores/library/js/main.js', array('jquery'), 'underscores', false);
 
 	wp_enqueue_script( 'underscores-navigation', get_template_directory_uri() . '/library/js/navigation.js', array(), '20151215', true );
 
